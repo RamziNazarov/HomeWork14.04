373de7ad9c3dbc7cd4639e7de138e77b702fa759
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-14 20:28:17 +0300

    create homework project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork14._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
